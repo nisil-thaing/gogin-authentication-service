934f83d22657005bea0a9b9a265a41acd79beb68
Thái (Nisil) Nguyên <11013123+nisil-thaing@users.noreply.github.com>
2025-06-09 19:39:47 +0700

    [FEATURE] - User registrations (#1)
    * Define User schemas, add routing, utils
    
    * Update user registration using transactions

---
+++ internal/models/auth.go
@@ -0,0 +1,14 @@
+package models
+
+type RegisteringUserSchema struct {
+	Email           string  `bson:"email" validate:"email,required"`
+	FirstName       *string `bson:"first_name" validate:"min=2,max=100"`
+	LastName        *string `bson:"last_name" validate:"min=2,max=100"`
+	Password        string  `bson:"password" validate:"required,min=8,eqfield=PasswordConfirm"`
+	PasswordConfirm string  `bson:"password_confirm" validate:"required"`
+}
+
+// type UserSigningInSchema struct {
+// 	Email    string `bson:"email" validate:"email,required"`
+// 	Password string `bson:"password" validate:"required"`
+// }
